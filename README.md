c9615a493d2466e0fa57bd879f23a8ad44a3b27f
Verhana <arrowfoxie@gmail.com>
2018-08-24 20:11:45 -0500

    text display edits & readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # mysql-storefront
+
+## Goodies 'r Us
+
+This is a pretend storefront application run from the terminal. It displays a list of items, which you can choose to purchase via the ID number and quantity. If there is none of that item left, you will get a message alerting you to pick a new item. If you successfully purchase an item, you will get a message finalizing the transaction, including the total cost of the transaction for the item.
+
+## Built using
+
+* [Node.js](https://nodejs.org/en/)
+* [MySQL](https://www.mysql.com/)
+* [Mysql npm package](https://www.npmjs.com/package/mysql)
+* [Inquirer](https://github.com/SBoudrias/Inquirer.js)
+ 
+## Instructions for Use
+
+0. Create a new database in mysql using the included bamazon.sql file
+1. Open folder in the command line
+2. type "npm install" to install dependencies
+3. type "node bamazonCustomer.js" to run the application
+
+## Live Demo
+
+* [Live Demo](https://drive.google.com/file/d/1uqgE9ivE8E0D7C-UhX_Ph-clWZrlZDmZ/view)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,18 +14,25 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     connection.query('SELECT * FROM `products`', function (err, res)
-{ 
-    for (var item of res){
-        console.log(item.product_name, item.price, item.item_id);
+{  
+      for (var item of res){
+        console.log("Item: " + item.product_name + "\n", 
+        "Price: " + item.price + "\n",
+        "Purchase ID: " + item.item_id + "\n",
+        "Quantity Available: " + item.stock_quantity);
+        console.log("~~~~~~~~~~~~~~~~~~");
     }
-
+    console.log("\x1b[44m", "\n" + "~~~~~~~~~~~~~~~~~ WELCOME TO GOODIES 'R US ~~~~~~~~~~~~~~~~~~~~~~~~~" + "\n", "\x1b[0m",
+    "Please see the item catalogue above for a complete list of the items we carry." + "\n")
     // run the start function after the connection is made to prompt the user
     start();
+    
 })
+
 });
 
 function start() {
-
+    console.log("\x1b[36m", "Please enter the ID number of the item you wish to purchase below.");
     inquirer
         .prompt([{
             name: "itemId",
@@ -55,7 +62,7 @@ function start() {
             var amount = answer.quantity;
             connection.query('SELECT * FROM `products` WHERE `item_id` = ?', [iD], function (err, res) {
                 if (res.length === 0) {
-                    console.log("The store is closed");
+                    console.log("\n", "Sorry. The store is experiencing technical difficulties.", "\n");
                     start(); 
                     return err;
                 }
@@ -72,13 +79,13 @@ function start() {
                         item_id: iD
                     }], function (error, response) {
                         // error handling
-                        console.log("you have purchased " + amount + " of " + product + " for $" + (price * amount));
+                        console.log("\x1b[32m", "\n", "You have purchased " + amount + " of " + product + " for $" + (price * amount), "\n", "\x1b[0m");
                         start();
                     });
                 } else {
                     // none available
                     if (stock === 0){
-                        console.log("There's no more");
+                        console.log("\x1b[31m", "\n" + "Sorry, there's no more in stock. Select another item." + "\n");
                     } else {
                     console.log("There's only " + stock + " left");
                 }
